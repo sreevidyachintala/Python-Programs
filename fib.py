1de84a093d9cbb57b4ca43f559f3538deb1fcb98
Sreevidya Chintala <48712054+sreevidyachintala@users.noreply.github.com>
2019-04-18 15:33:30 +0530

    upload file

---
+++ fib.py
@@ -0,0 +1,26 @@
+
+# Program to display the Fibonacci sequence
+
+# change this value for a different result
+n = int(input("enter n value"))
+
+# first two terms
+n1 = 0
+n2 = 1
+count = 0
+
+# check if the number of terms is valid
+if n <= 0:
+   print("Please enter a positive integer")
+elif n == 1:
+   print("Fibonacci sequence upto",n,":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto",n,":")
+   while count < n:
+       print(n1,end=' , ')
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count = count+1
